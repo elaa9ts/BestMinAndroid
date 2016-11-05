967b425ea60fb3ef40141f7034c728ea4f94480d
elaas <elavarasan@ndtv.com>
2016-11-05 21:18:29 +0530

    Navigation added

--- BestMin/app/src/main/java/com/hari/bestmin/mvvm/view/activity/MainActivity.java
+++ BestMin/app/src/main/java/com/hari/bestmin/mvvm/view/activity/MainActivity.java
@@ -1,15 +1,30 @@
 package com.hari.bestmin.mvvm.view.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 
 import com.hari.bestmin.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerToggle.syncState();
+        drawerLayout.addDrawerListener(drawerToggle
+        );
+
     }
 }
