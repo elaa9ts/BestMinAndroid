850b617c607668d4f9dffd1a5f490349883794af
elaas <elavarasan@ndtv.com>
2016-11-05 20:06:31 +0530

    Initial Commit

---
+++ BestMin/app/src/main/java/com/hari/bestmin/mvvm/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hari.bestmin.mvvm.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hari.bestmin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
