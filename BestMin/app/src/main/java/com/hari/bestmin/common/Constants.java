0b7132dcf4a49963832c90b842e32957ab378f34
elaas <elavarasan@ndtv.com>
2016-11-05 21:30:58 +0530

    packages created

---
+++ BestMin/app/src/main/java/com/hari/bestmin/common/Constants.java
@@ -0,0 +1,8 @@
+package com.hari.bestmin.common;
+
+/**
+ * Created by ELAA on 05-11-2016.
+ */
+
+public class Constants {
+}
---
+++ BestMin/app/src/main/java/com/hari/bestmin/mvvm/model/Item.java
@@ -0,0 +1,10 @@
+package com.hari.bestmin.mvvm.model;
+
+/**
+ * Created by ELAA on 05-11-2016.
+ */
+
+public class Item {
+
+    public int total;
+}
